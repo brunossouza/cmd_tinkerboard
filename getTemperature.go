94b11ea6d35e24d289058e01ff6aa634531be34c
Bruno Silva <brunos.souza2@gmail.com>
2020-02-17 17:23:35 -0300

    get temperature

---
+++ getTemperature.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strconv"
+	"time"
+)
+
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getTemperature() {
+	dat, err := ioutil.ReadFile("/sys/class/thermal/thermal_zone0/temp")
+	check(err)
+	tempStr := string(dat)
+	temp, err := strconv.Atoi(tempStr[:len(tempStr)-1])
+	check(err)
+	temp = temp / 1000
+	fmt.Println("Temperatura da CPU:\t", temp, "°C")
+}
+
+func main() {
+	var watchTime int
+	flag.IntVar(&watchTime, "w", 0, "tempo(em segundos) de entre cada interação do comando.")
+
+	flag.Parse()
+	watch := watchTime > 0
+	if !watch {
+		getTemperature()
+	}
+	for watch {
+		print("\033[H\033[2J")
+		fmt.Print(time.Now().Format("2006-01-02 15:04:05"), "\n\n")
+		getTemperature()
+		time.Sleep(time.Duration(watchTime) * time.Second)
+	}
+
+}
